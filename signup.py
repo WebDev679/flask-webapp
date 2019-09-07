3eab0ad32d79667b079dc4eefcbded8bf6ca10e7
arnav.679.2022 <arnav.679.2022@penguin>
2019-09-07 10:36:09 +0530

    Added the original page need to work on db

--- signup.py
+++ signup.py
@@ -1,15 +1,51 @@
-from flask import Flask as flask
-from flask import redirect, url_for, render_template, request
 import MySQLdb
-
+from flask import Flask as flask
+from flask import redirect, url_for, request, render_template
+import login
+import pymysql
 app = flask(__name__, template_folder = 'template')
-connection = MySQLdb.connect('localhost','root', '')
-@app.route('/signup', methods = ['GET', 'POST'])
-def signup():
-    cursor = connection.cursor()
-    if request.method == 'POST':
-        name  = request.form('username')
-        passw = request.form('password')
-        email = request.form('email')
 
-    return render_template('index.html')
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def siggitgit vnup():
+    connection = MySQLdb.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = ''
+    )
+    con = connection.cursor()
+    con.execute('use Project')
+    if request.method == 'POST':
+        name = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        email1 = "SELECT * from users WHERE email = " + "'" + email + "'"
+        con.execute(email1)
+        row = con.fetchone()
+        if row is not None:
+            return 'Email already taken'
+            return redirect(url_for('login'))
+        username1 = "SELECT * FROM users WHERE name = "  + "'"+ name + "'"
+        con.execute(username1)
+        row1 = con.fetchone()
+        if row1 is not None:
+            return 'Username already taken'
+            return redirect(url_for('signup'))
+
+        stmt = "INSERT INTO users (name, email, password) VALUES (" + "'" + name + "'" + ", " + "'" +  email + "'" + ", " + "'" +   password+ "'" + ")"
+        print(stmt)
+        con.execute(stmt)
+        con.execute("SELECT * FROM users")
+        cols = con.fetchall()
+        print(cols)
+        f
+
+
+
+
+
+    return render_template('sign_up.html')
+    return redirect(url_for('/login'))
+if __name__ == '__main__':
+    app.debug = True
+    app.run(debug = True)
