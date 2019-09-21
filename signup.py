c3c1de31db1e7227976cec32e6df21c6876ee120
root <root@penguin>
2019-09-21 20:49:32 +0530

    Added login file and successfully made the sgnup portal erro bugs still remaining

--- signup.py
+++ signup.py
@@ -1,50 +1,43 @@
 import MySQLdb
 from flask import Flask as flask
 from flask import redirect, url_for, request, render_template
-import pymysql
+import MySQLdb
 app = flask(__name__, template_folder = 'template')
 
 
 @app.route('/signup', methods = ['GET', 'POST'])
-def sigup():
-    connection = MySQLdb.connect(
-    host = 'localhost',
-    user = 'root',
-    passwd = ''
-    )
+def signup():
+    connection = MySQLdb.connect('localhost', 'root', '25091813', 'Project')
     con = connection.cursor()
-    con.execute('use Project')
     if request.method == 'POST':
-        name = request.form.get('username')
-        password = request.form.get('password')
-        email = request.form.get('email')
-        email1 = "SELECT * from users WHERE email = " + "'" + email + "'"
-        con.execute(email1)
+        name = request.form.['username']
+        password = request.form.['password']
+        email = request.form.['email']
+        email_sql = "SELECT * from users WHERE email = " + "'" + email + "'"
+        email_sql = f"SELECT * from users WHERE email = '{email}'"
+        con.execute(email_sql)
         row = con.fetchone()
         if row is not None:
-            return 'Email already taken'
-            return redirect(url_for('login'))
-        username1 = "SELECT * FROM users WHERE name = "  + "'"+ name + "'"
-        con.execute(username1)
+            error = 'r3c'
+            render_template('index.html', error = error)
+
+        username_sql = "SELECT * FROM users WHERE name = "  + "'"+ name + "'"
+        con.execute(username_sql)
         row1 = con.fetchone()
         if row1 is not None:
-            return 'Username already taken'
-            return redirect(url_for('signup'))
+            error = 'r3c'
+            render_template('index.html', error = error)
 
-        stmt = "INSERT INTO users (name, email, password) VALUES (" + "'" + name + "'" + ", " + "'" +  email + "'" + ", " + "'" +   password+ "'" + ")"
-        print(stmt)
-        con.execute(stmt)
-        con.execute("SELECT * FROM users")
-        cols = con.fetchall()
-        print(cols)
-        f
+        if row1 is None and row is None:
+            stmt = "INSERT INTO users (name, email, password) VALUES (" + "'" + name + "'" + ", " + "'" +  email + "'" + ", " + "'" +   password+ "'" + ")"
+            print(stmt)
+            con.execute(stmt)
+            connection.commit()
+            con.execute("SELECT * FROM users")
+            cols = con.fetchall()
+            print(cols)
 
-
-
-
-
-    return render_template('sign_up.html')
-    return redirect(url_for('/login'))
+    return render_template('index.html')
 if __name__ == '__main__':
     app.debug = True
     app.run(debug = True)
